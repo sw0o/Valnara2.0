2eba3a6728327c73a02091ec86db50e6186a59df
Zain <hzain1122334455@gmail.com>
2025-01-12 14:53:32 +0300

    wordpress detection

--- Modules/WordPress_Detection.py
+++ Modules/WordPress_Detection.py
@@ -1,2 +1,12 @@
-import beautifulsoup4  
 import requests
+
+def Is_WordPress(url):
+    Api_Key = "w43u2hdtv0tlmbb60321aakv8zbvqj0k2f682afp1y3r7b6rlrq0262bd250ibho20zsa6"
+    Api_Url = f"https://whatcms.org/APIEndpoint/Detect?key={Api_Key}&url={url}"
+    response = requests.get(Api_Url).json()
+    if response.get('result', {}).get('name') == "WordPress":
+        return "WordPress Detected"
+    else:
+        return "WordPress Not Detected"
+
+
--- Modules/input.py
+++ Modules/input.py
@@ -3,13 +3,14 @@ import re
 from rich.console import Console
 from rich.prompt import Prompt
 from Banner import display_banner
+from WordPress_Detection import Is_WordPress    
 c = Console()
 
 display_banner()
 
 def Valid_Format(URL): #
     Blocked_Domains = {".mil", ".gov", ".edu"}
-    r = r"^(https?://)([a-zA-Z0-9\u00a1-\uffff-]+\.)+(com|org|net|edu|gov|mil|info|biz|name|pro|xyz|xn--[a-zA-Z0-9-]+\.[a-zA-Z]{2,})(\/[^\s]*)?$"
+    r = r"^(https?://)([a-zA-Z0-9\u00a1-\uffff-]+\.)+(com|org|net|edu|tech|gov|mil|info|biz|name|pro|xyz|xn--[a-zA-Z0-9-]+\.[a-zA-Z]{2,})(\/[^\s]*)?$"
     if any(domain in URL for domain in Blocked_Domains):
         c.print("[bold red]Blocked Domain[/bold red]")    
         return "Blocked Domain"
@@ -24,7 +25,7 @@ def Check_Presence(url):  #
     try:
         response = requests.get(url)
         if 200 <= response.status_code < 300:
-            c.print("[green]URL is reachable[/green]")
+            return  "Website is reachable"
         else: 
             c.print("[red]URL is not reachable[/red]")
     except requests.exceptions.Timeout:
@@ -40,7 +41,10 @@ try:
 except KeyboardInterrupt: 
     print("\nKeyboardInterrupt has been caught.")
 
-
+################### controllers ###################
 if Valid_Format_Result == "Valid URL":
-    Check_Presence(URL)
-
+    Precense_Result = Check_Presence(URL)
+    if Precense_Result == "Website is reachable":
+        WordPress_Result = Is_WordPress(URL)
+        if WordPress_Result == "WordPress Not Detected":
+            c.print("[bold green]no WordPress Detected[/bold green]")
